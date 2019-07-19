798631904650de5f3c38cfcb6f442f2c167241c3
AliAnilKocak <alianilkocak@gmail.com>
2019-07-19 11:05:49 +0300

    route fixed

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,11 @@
 
 Route::get('/about', 'HomePageController@about')->name('about');
 Route::get('/contact', 'HomePageController@contact')->name('contact');
-
+Route::get('/{slug_postname}', 'PostController@index')->name('post');
 
 Route::get('/{navbar_slug}', 'NavbarController@index')->name('navbar');
 Route::get('/', 'HomePageController@index')->name('homepage');
-Route::get('/{slug_postname}', 'PostController@index')->name('post');
+
 Route::get('/tag/{slug_tagname}', 'TagController@index')->name('tag');
 Route::get('/category/{slug_categoryname}', 'CategoryController@index')->name('category');
 
